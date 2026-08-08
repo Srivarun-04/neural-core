8d82019395f6f748833864ca6ebbbc0fd7700f84
Srivarun-04 <manthenasrivarun@gmail.com>
2026-08-08 10:14:36 +0530

    Added Requirement file

--- main.py
+++ main.py
@@ -60,5 +60,8 @@ def read_root():
     }
 
 if __name__ == "__main__":
+    import os
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    host = os.getenv("HOST", "0.0.0.0")
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("main:app", host=host, port=port, reload=False)
